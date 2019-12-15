52615eef1b2d1903e939eff7271053aafe041b2d
QiangQiang-77 <1084844653@qq.com>
2019-12-15 22:16:17 +0800

    加入了JPA的依赖

--- src/main/java/javaweb/forum/controller/UserController.java
+++ src/main/java/javaweb/forum/controller/UserController.java
@@ -1,5 +1,6 @@
 package javaweb.forum.controller;
 
+import javaweb.forum.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/javaweb/forum/service/UserService.java
+++ src/main/java/javaweb/forum/service/UserService.java
@@ -1,5 +1,6 @@
 package javaweb.forum.service;
 
+import javaweb.forum.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
